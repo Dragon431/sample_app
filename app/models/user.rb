30f1d3ebdd3c6fb47aebaaed5610cfd05c17f88e
Dragon431 <wingzero431@gmail.com>
2013-05-29 19:38:25 -0500

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,8 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
   
-  before_save { email.downcase! }
+  before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
   
   validates :name, 	
   		presence: true, 
@@ -32,4 +33,9 @@ class User < ActiveRecord::Base
   		
   validates :password_confirmation,
   		presence: true
+  		
+  private
+		def create_remember_token
+			self.remember_token = SecureRandom.urlsafe_base64
+		end
 end
